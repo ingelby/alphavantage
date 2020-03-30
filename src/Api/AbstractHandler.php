79184c60e5960857e56e94717e053582d1f82507
George Hallam <george@ingelby.com>
2020-03-30 20:24:50 +0000

    Added constructor

--- src/Api/AbstractHandler.php
+++ src/Api/AbstractHandler.php
@@ -6,5 +6,8 @@ use ingelby\toolbox\services\InguzzleHandler;
 
 class AbstractHandler extends InguzzleHandler
 {
-
+    public function __construct($baseUrl, $uriPrefix = '', callable $clientErrorResponseCallback = null, callable $serverErrorResponseCallback = null)
+    {
+        parent::__construct($baseUrl, $uriPrefix, $clientErrorResponseCallback, $serverErrorResponseCallback);
+    }
 }
